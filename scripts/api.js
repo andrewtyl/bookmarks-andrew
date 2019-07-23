691ebf5e10eafde292d3aab843dd1b68fcca7b75
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-23 02:34:16 -0500

    css and api work

--- scripts/api.js
+++ scripts/api.js
@@ -13,7 +13,66 @@ const api = (function() {
         })
     }
 
+    const addBookmark = (title, url, desc, rating) => {
+        let upload = {
+            title: title,
+            url: url,
+            desc: desc,
+            rating: rating,
+        }
+        return fetch(baseURL, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {'Content-Type': 'applcation/json'},
+            body: JSON.stringify(upload)
+        })
+        .then(res => {
+            if (!res.ok) {
+                let error = "There was an issue adding your bookmark. Please refresh the page and try again.";
+                store.errorMessage = error;
+                store.showError = true;
+            }
+            else {
+                return res.json();
+            }
+        })
+    }
+
+    const editBookmark = (title, url, desc, rating) => {
+        let upload = {
+            title: title,
+            url: url,
+            desc: desc,
+            rating: rating,
+        }
+        return fetch(baseURL, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {'Content-Type': 'applcation/json'},
+            body: JSON.stringify(upload)
+        })
+        .then(res => {
+            if (!res.ok) {
+                let error = "There was an issue adding your bookmark. Please refresh the page and try again.";
+                store.errorMessage = error;
+                store.showError = true;
+            }
+            else {
+                return res.json();
+            }
+        })
+    }
+
+    }
+
+    const deleteBookmark = () => {
+
+    }
+
     return {
-        getBookmarks
+        getBookmarks,
+        addBookmark,
+        editBookmark,
+        deleteBookmark
     };
 })()
